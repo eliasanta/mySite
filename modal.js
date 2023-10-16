4e934e094b701b7481530ed9607c86c82e2bb395
Elia Santagiuliana <eliasanta93@gmail.com>
2023-10-16 12:28:39 +0200

    update

--- modal.js
+++ modal.js
@@ -1,3 +1,20 @@
-$("#myModal").on("shown.bs.modal", function () {
+/* $("#myModal").on("shown.bs.modal", function () {
   $("#myInput").trigger("focus");
+}); */
+// Trova il modal e aggiungi un evento quando viene mostrato
+document.addEventListener("DOMContentLoaded", function () {
+  var modal = document.getElementById("myModal");
+
+  // Assicurati che il modal esista prima di aggiungere l'evento
+  if (modal) {
+    modal.addEventListener("shown.bs.modal", function () {
+      // Trova l'elemento "myInput" all'interno del modal e dategli il focus
+      var myInput = document.getElementById("myInput");
+      if (myInput) {
+        myInput.focus();
+      }
+    });
+  }
 });
+
+
